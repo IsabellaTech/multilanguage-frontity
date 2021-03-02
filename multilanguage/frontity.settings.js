bbc5fddec55c43e6000276ccec5115e0789f1fab
Alejandro Villalobos <alevillalobos.1993@gmail.com>
2021-03-02 14:50:34 +0100

    commit initial

--- multilanguage/frontity.settings.js
+++ multilanguage/frontity.settings.js
@@ -1,57 +1,156 @@
-const settings = {
-  "name": "multilanguage",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
-  },
-  "packages": [
-    {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
+const settings = [
+  {
+    "name": "multilanguage-en",
+    "state": {
+      "frontity": {
+        "url": "https://alejandro.inmo.cloud/",
+        "title": "Test Frontity Blog ENGLISH",
+        "description": "WordPress installation for Frontity development"
+      }
+    },
+    "packages": [
+      {
+        "name": "@frontity/mars-theme",
+        "state": {
+          "theme": {
+            "lang": "en",
+            "menu": [
+              {
+                "name": "Home ENG",
+                "url": "/",
+              },
+              {
+                "name": "Page Test ENG",
+                "url": "/page/",
+              },
+              {
+                "name": "Post ENG",
+                "url": "/hello-world/"
+              },
+              {
+                "name": "|",
+                "url": "#"
+              },
+              {
+                "languages": [{ "lang": "ESP", "url": "/es/" }],
+              },
+              {
+                "name": "|",
+                "url": "#"
+              },
+              {
+                "name": "Home ES",
+                "url": "/es/"
+              },
+              {
+                "name": "Page Test ES",
+                "url": "/es/page/"
+              },
+              {
+                "name": "Post ES",
+                "url": "/es/hello-world/"
+              },
+
             ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
-          ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
+            "featured": {
+              "showOnList": false,
+              "showOnPost": false
+            }
           }
         }
-      }
-    },
-    {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://test.frontity.org"
+      },
+      {
+        "name": "@frontity/wp-source",
+        "state": {
+          "source": {
+            "api": "https://alejandro.inmo.cloud/multilenguage/wp-json",
+            "homepage": "homepage",
+          }
         }
+      },
+      "@frontity/tiny-router",
+      "@frontity/html2react"
+    ]
+  },
+
+  //SPANISH VERSION
+  {
+    "name": "multilanguage-es",
+    "match": [".*localhost:3000/es(\/.*)?$"],
+    "state": {
+      "frontity": {
+        "url": "https://alejandro.inmo.cloud/es/",
+        "title": "Test Frontity Blog ESPAÑOL",
+        "description": "WordPress instalacion en frontity"
       }
     },
-    "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
-};
+    "packages": [
+      {
+        "name": "@frontity/mars-theme",
+        "state": {
+          "theme": {
+            "lang": "es",
+            "menu": [
+
+              {
+                "name": "Home ENG",
+                "url": "/",
+              },
+              {
+                "name": "Page Test ENG",
+                "url": "/page/",
+              },
+              {
+                "name": "Post ENG",
+                "url": "/hello-world/"
+              },
+              {
+                "name": "|",
+                "url": "#"
+              },
+              {
+                "languages": [{ "lang": "ENG", "url": "/en/" }],
+              },
+              {
+                "name": "|",
+                "url": "#"
+              },
+              {
+                "name": "Home ES",
+                "url": "/es/"
+              },
+              {
+                "name": "Page Test ES",
+                "url": "/es/page/"
+              },
+              {
+                "name": "Post ES",
+                "url": "/es/hello-world/"
+              },
+
+
+            ],
+            "featured": {
+              "showOnList": false,
+              "showOnPost": false
+            }
+          }
+        }
+      },
+      {
+        "name": "@frontity/wp-source",
+        "state": {
+          "source": {
+            "api": "https://alejandro.inmo.cloud/multilenguage/es/wp-json",
+            "homepage": "homepage",
+          }
+        }
+      },
+      "@frontity/tiny-router",
+      "@frontity/html2react"
+    ]
+  }
+
+];
 
 export default settings;
--- multilanguage/packages/mars-theme/src/components/nav.js
+++ multilanguage/packages/mars-theme/src/components/nav.js
@@ -1,75 +1,137 @@
+import React, { useForm } from "react";
 import { connect, styled } from "frontity";
 import Link from "./link";
-
 /**
  * Navigation Component
  *
  * It renders the navigation links
  */
-const Nav = ({ state }) => (
-  <NavContainer>
-    {state.theme.menu.map(([name, link]) => {
-      // Check if the link matched the current page url
-      const isCurrentPage = state.router.link === link;
-      return (
-        <NavItem key={name}>
-          {/* If link url is the current page, add `aria-current` for a11y */}
-          <Link link={link} aria-current={isCurrentPage ? "page" : undefined}>
-            {name}
-          </Link>
-        </NavItem>
-      );
-    })}
-  </NavContainer>
-);
+const Nav = ({ state, libraries, actions }) => {
+  //const items = state.source.get(`/menu/${state.theme.menuUrl}/`)?.items;
+  const items = state.theme.menu;
+  const url = state.router.link;
+  console.log(items)
+  const options_lang = [
+    { type: 'inputLanguage', value: 'en', label: 'English' },
+    { type: 'inputLanguage', value: 'es', label: 'Español' },
+  ]
+
+  const handleInputChangeUrl = (e) => {
+    console.log(e.target.baseURI);
+    console.log(e.target.value);
+    let newUrl = e.target.baseURI
+
+    options_lang.map(({ value }, id) =>
+      e.target.baseURI.includes(`/${value}/`) == true && (
+        newUrl = newUrl.replace(value, e.target.value)
+      )
+    );
+
+    actions.router.set(newUrl);
+
+  }
+
+  return (
+    <MenuContainer>
+      {items?.map((item, id) => {
+        if (!item.child_items && !Array.isArray(item.languages)) {
+          return (
+            <MenuItem key={id} className={url == item.url ? "active-page" : ''}>
+              <Link link={item.url}> {item.name} </Link>
+            </MenuItem>
+          );
+        } else if (Array.isArray(item.languages)) {
+          const languages = item.languages;
+          return (
+            <NavItemWithChild key={id}>
+              <MenuItem key={id}>
+                <Link link='#'> LANGUAGE </Link>
+              </MenuItem>
+
+
+              <ChildMenuDropdown className="dropdown">
+                {languages.map((language, id) => {
+                  return (
+                    <a key={id} href={language.url}>{language.lang}</a>
+                  );
+                })}
+              </ChildMenuDropdown>
+            </NavItemWithChild>
+          );
+        }
+        else {
+          const childItems = item.child_items;
+          return (
+            <NavItemWithChild key={id}>
+
+              <MenuItem key={id} className={url == item.url ? "active-page" : ''}>
+                <Link link={item.url}> {item.name} </Link>
+              </MenuItem>
+
+              <ChildMenuDropdown className="dropdown">
+                {childItems.map((childItem, id) => {
+                  return (
+                    <Link key={id} link={childItem.url}>{childItem.name}</Link>
+                  );
+                })}
+              </ChildMenuDropdown>
+            </NavItemWithChild>
+          );
+        }
+
+
+      })}
+
+
+
+    </MenuContainer>
+  );
+};
 
 export default connect(Nav);
 
-const NavContainer = styled.nav`
-  list-style: none;
+const MenuContainer = styled.div`
   display: flex;
-  width: 848px;
-  max-width: 100%;
-  box-sizing: border-box;
-  padding: 0 24px;
-  margin: 0;
-  overflow-x: auto;
-
-  @media screen and (max-width: 560px) {
-    display: none;
+  justify-content:space-around;
+  a:hover{
+    text-decoration: underline;
+    text-decoration-color: white;
   }
 `;
-
-const NavItem = styled.div`
-  padding: 0;
-  margin: 0 16px;
-  color: #fff;
-  font-size: 0.9em;
-  box-sizing: border-box;
-  flex-shrink: 0;
-
+const MenuItem = styled.div`
+  font-size: 1.5rem;
   & > a {
-    display: inline-block;
-    line-height: 2em;
-    border-bottom: 2px solid;
-    border-bottom-color: transparent;
+    padding: 20px 20px;
     /* Use for semantic approach to style the current link */
-    &[aria-current="page"] {
-      border-bottom-color: #fff;
-    }
+   
   }
-
-  &:first-of-type {
-    margin-left: 0;
+  color:white;
+`;
+const NavItemWithChild = styled.div`
+  position: relative;
+  display: inline-block;
+  
+  :hover .dropdown{
+    display:block;
   }
-
-  &:last-of-type {
-    margin-right: 0;
-
-    &:after {
-      content: "";
-      display: inline-block;
-      width: 24px;
-    }
+  
+`;
+const ChildMenuDropdown = styled.div`
+  display: none;
+  position: absolute;
+  background-color: black;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  top: 32px;
+  a{
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    color:white;
+  }
+  a:hover {
+    background-color: white;
+    color:black;
   }
 `;
